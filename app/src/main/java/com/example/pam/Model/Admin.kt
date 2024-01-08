ec624480821b2faca7e7c9093ca3e5287838d242
febyan <febyan.putra.ft21@mail.umy.ac.id>
2024-01-08 17:03:51 +0700

    halo

--- app/src/main/java/com/example/pam/Model/Admin.kt
+++ app/src/main/java/com/example/pam/Model/Admin.kt
@@ -7,3 +7,23 @@ data class Admin(
 ){
     constructor(): this("","","",)
 }
+data class makanan(
+    val id: String,
+    val namamkn: String,
+    val harga: String,
+    val jenis: String
+){
+    constructor(): this("","","","")
+}
+
+data class struck(
+    val id: String,
+    val nama: String,
+    val password: String,
+    val namamkn: String,
+    val harga: String,
+    val jenis: String
+){
+    constructor(): this("","","","","","",)
+
+}
